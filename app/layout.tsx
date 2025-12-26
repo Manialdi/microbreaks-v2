1664edbab1fd880fc4fe00f75c751806f895e277
Manikanth Aldi <manialdi9999@gmail.com>
2025-12-26 18:53:35 +0530

    feat: add linkedin insight tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,30 @@ export default function RootLayout({
             gtag('config', 'G-D64BWV2LB4');
           `}
         </Script>
+        <Script id="linkedin-insight" strategy="afterInteractive">
+          {`
+            _linkedin_partner_id = "8472356";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+            (function(l) {
+            if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+            window.lintrk.q=[]}
+            var s = document.getElementsByTagName("script")[0];
+            var b = document.createElement("script");
+            b.type = "text/javascript";b.async = true;
+            b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+            s.parentNode.insertBefore(b, s);})(window.lintrk);
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=8472356&fmt=gif"
+          />
+        </noscript>
         {children}
       </body>
     </html>
